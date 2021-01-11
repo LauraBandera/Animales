949340b53ae0b01af6a9e0acfb998f92340aa4b1
Laura <laurabande10@gmail.com>
2021-01-11 17:59:14 +0100

    Creación clase Persona con los atributos de clase y constructores

---
+++ src/main/java/daw/Laura/Persona.java
@@ -0,0 +1,17 @@
+package daw.Laura;
+
+public class Persona {
+
+	private String nombre;
+	private int edad;
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public Persona() {
+		super();
+	}
+}
