691c5a7f9077dd4389a82340e72e49b23ed50bc6
Laura <laurabande10@gmail.com>
2021-01-11 18:06:08 +0100

    Métodos restantes en clase Persona

--- src/main/java/daw/Laura/Persona.java
+++ src/main/java/daw/Laura/Persona.java
@@ -14,4 +14,21 @@ public class Persona {
 	public Persona() {
 		super();
 	}
+	
+	public void llamar(Animal pet) {
+		pet.despertar();
+	}
+	
+	public void alimentar(Animal pet, double cantidadGramos) {
+		pet.comer(cantidadGramos);
+	}
+	
+	public void jugar(Animal pet, int cantidadMinutos) {
+		if(cantidadMinutos > 180) {
+			throw new IllegalArgumentException ("Valor introducido no válido");
+			//cantidadMinutos = 180;
+		}
+		cantidadMinutos = 180;
+		pet.jugar(cantidadMinutos);
+	}
 }
