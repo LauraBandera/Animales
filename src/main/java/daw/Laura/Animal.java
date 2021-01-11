4566a2c9a781599746f746508758515dca8c6f61
Laura <laurabande10@gmail.com>
2021-01-11 14:35:59 +0100

    Creación de atributos y constructores

--- src/main/java/daw/Laura/Animal.java
+++ src/main/java/daw/Laura/Animal.java
@@ -5,10 +5,43 @@
  */
 package daw.Laura;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author laura
  */
 public class Animal {
+
+    /*Información de referencia
+      DateTimeFormatter
+      LocalDate.of
+    */
+    
+    private LocalDate nacimiento;
+    private String nombre;
+    private int tipo, //1 = Gato, 2 = Perro, 3 = Lagarto, 4 = Cobaya, 5 = Periquito
+            estado; //1 = Comiendo, 2 = Durmiendo, 3 = Despierto/reposo, 4 = Jugando
+    private double peso;
+
+    public Animal() {
+        this.tipo = 1;
+        this.nombre = "Turrón";
+        this.peso = 2200;
+        this.estado = 2;
+        this.nacimiento = LocalDate.now();
+        
+    }
+
+    public Animal(LocalDate nacimiento, String nombre, int tipo, int estado, double peso) {
+        this.nacimiento = nacimiento;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.estado = estado;
+        this.peso = peso;
+    }
+    
+    
+    
     
 }
